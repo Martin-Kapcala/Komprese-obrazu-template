fa6a048e63bd243284601fd84248336b55af5724
Martin-Kapcala <martin.kapcala@vitkovickastredni.cz>
2024-02-10 19:16:13 +0100

    Initial commit

---
+++ KompreseObrazu/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Komprese
+{
+    public class KompreseObrazu{
+
+        public static void Main (String [] args){
+        
+        //vytvoření instance třídy Obrazek
+        Obrazek inputCSV = new Obrazek();
+
+        //Cesta k testovacímu souboru
+        String testFilePath = @"C:\Users\MartinKapcala\OneDrive - Vítkovická střední průmyslová škola\Dokumenty\Programování\PG\Komprese-obrazu\KompreseObrazu\CSV\obr1-10.csv";
+        
+        //Test metody, která načte vstupní obrázek
+        inputCSV.readImg(testFilePath);
+        
+        //Test metody, která spočítá počet řádků vstupního obrázku
+        Console.WriteLine("Počet vertikálních řádků {0}",inputCSV.CountLines(testFilePath));
+        
+        //Test metody, která spočítá počet symbolů vstupního obrázku v prvním řádku
+        Console.WriteLine("Počet horizontální řádků {0}",inputCSV.CountSymbolInLine(testFilePath));
+
+        inputCSV.vypisImg();
+    }
+
+    }
+    
+    
+}
